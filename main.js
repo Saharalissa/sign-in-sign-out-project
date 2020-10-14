6587de27f923fa91309159db6b28e463705fe15a
Saharalissa <sahar.alissa@gmail.com>
2020-10-15 00:23:30 +0300

    adding alert functionality

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
+var contact = [];
 
-$('.btn-default').click(function () {
-	var firstName=$("fname").val()
-	var lastName=$("lname").val();
-	var pass=$("pass").val();
-	var cpass=$("cpass").val();
-	var email=$("email").val();
+	
+// alert message in the registration page
+$('#button').click(function() {
+	var data = {};
+	var firstName=$("#fname").val();
+	var lastName=$("#lname").val();
+	var pass=$("#pass").val();
+	var cpass=$("#cpass").val();
+	var email=$("#email").val();
 
-	if (firstName ===undefined || lastName ===undefined || pass ===undefined  || cpass ===undefined || email ===undefined) {
+	if (firstName ==="" && lastName ==="" && pass ===""  && cpass ==="" && email ==="") {
 		alert("PLEASE FILL ALL REQUIRED FIELDS")
-	}
-})
+	} else if (firstName !=="" && lastName !=="" && pass === cpass && email !=="" ) {
+		// alert("YOU HAVE SUCCESSFULLY SIGNED IN");
+		data.Firstname = firstName;
+        data.Lastname = lastName;
+        data.Pass = pass;
+        data.Email = email;
+
+        contact.push(data);
+		}
+		})
+		
+
+// alert message in the sign-in page
+$('#button2').click(function() {
+	var userName=$("#user").val();
+	var password=$("#password").val();
+	if (userName ==="" && password ==="") {
+		alert("PLEASE FILL ALL REQUIRED FIELDS")
+	} else {
+		alert("YOU HAVE SUCCESSFULLY SIGNED IN")
+		}	
+	})
+
 
