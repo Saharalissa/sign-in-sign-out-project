8f3bd8b3483869625c9fa206d94ac0f4b4f3c94d
Saharalissa <sahar.alissa@gmail.com>
2020-10-14 11:55:03 +0300

    adding a new html page for sign in form

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+function check() {
+	var firstName=document.getElementById("fname");
+	var lastName=document.getElementById("lname");
+	var pass=document.getElementById("pass");
+	var cpass=document.getElementById("cpass");
+	var email=document.getElementById("email");
+
+	
+	if (firstName.value==="" || lastName.value==="" || pass.value==="" || cpass.value==="" email.value==="") {
+		alert("PLEASE SELECT GENDER")
+	}
+}
